d34f642879aad8deecfb21b2082886bc4e3b69ae
YoungCheon <kakao_story@naver.com>
2022-11-13 22:00:04 +0900

    11/13

--- components/NavBar.js
+++ components/NavBar.js
@@ -3,10 +3,18 @@ import { useRouter } from "next/router";
 import styles from "./NavBar.module.css";
 
 export default function NavBar() {
-	return (
-		<nav className={styles.nav}>
-			<Link href="/">HOME</Link>
-			<Link href="/about">ABOUT</Link>
-		</nav>
-	);
+    return (
+        <nav className={styles.nav}>
+            <Link href="/">HOME</Link>
+            <Link href="/about">ABOUT</Link>
+            <style jsx>{`
+                nav {
+                    background-color: tomato;
+                }
+                a {
+                    text-decoration: none;
+                }
+            `}</style>
+        </nav>
+    );
 }
